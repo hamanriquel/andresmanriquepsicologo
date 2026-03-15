5f066c5c7264aa5a700d4d04c7fbc026767207df
Andres Manrique <161509365+hamanriquel@users.noreply.github.com>
2026-03-15 09:56:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -153,5 +153,59 @@ document.querySelectorAll(".faq-question").forEach(question => {
 });
 </script>
 
+<script>
+
+const counters = document.querySelectorAll('.counter');
+
+const startCounter = (counter) => {
+
+    const target = +counter.getAttribute('data-target');
+    let count = 0;
+
+    const speed = target / 120;
+
+    const update = () => {
+
+        count += speed;
+
+        if(count < target){
+
+            counter.innerText = Math.floor(count);
+            requestAnimationFrame(update);
+
+        }else{
+
+            counter.innerText = target;
+
+        }
+
+    };
+
+    update();
+};
+
+
+const observer = new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            const counter = entry.target;
+            startCounter(counter);
+            observer.unobserve(counter);
+
+        }
+
+    });
+
+},{threshold:0.6});
+
+
+counters.forEach(counter => {
+    observer.observe(counter);
+});
+
+</script>
 
 
